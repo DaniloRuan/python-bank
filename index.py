4f8c537344a422185e15044d8825fdabd1a41fa5
DaniloR <98754038+DaniloRuan@users.noreply.github.com>
2024-12-25 13:20:16 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
+#importando bibliotecas
 import os 
 import time
 
+#criando variariaveis
 nome = None
 cpf = None
 saldo = 0
 
+#dados a serem inseridos para acessar o sistema
 os.system("clear")
 user_name = input("coloque seu nome: ")
 user_cpf = input("coloque seu CPF: ")
@@ -13,19 +16,22 @@ name = user_name
 cpf = user_cpf
 
 
-
+#inicio do sistema
 while True:
 
+    #funcao que pega o valor que o usuario insere para fazer consultas, saque, deposito
     def verify(valor_user):
 
         number_conversor = int(valor_user)
 
+        #consulta da variavel saldo
         if number_conversor == 1:
             os.system("clear")
             print(f"o seu saldo atual: {saldo} R$")
             time.sleep(1)
             return "consulta"
 
+        #sistema para Deposito
         elif number_conversor == 2:
             os.system("clear")
             user_deposit = int(input("Coloque a quantia: "))
@@ -33,6 +39,8 @@ while True:
             print(f"Você depositou {user_deposit} R$")
             time.sleep(1)
             return "deposit", user_deposit
+
+        #sistema de Saque
         elif number_conversor == 3:
             os.system("clear")
             user_saque = int(input("Quantia de Saque: "))
@@ -43,22 +51,28 @@ while True:
             
 
 
-
+    #validador dos dados do cliente caso esteje vazio
     if name == "" or cpf == "":
         os.system("clear")
         print("Preencha os campos")
         time.sleep(4)
+    
     else:
+        #sistema que pega o valor do usuario para leva-lo há funcao a cima
         os.system("clear")
         print("1. Consultar Saldo")
         print("2. Depositar")
         print("3. Sacar")
         user_value = input("coloque um numero: ")
+
+        #a fucao retorna 2 valores, o tipo de valor e o valor em si
         deposit = verify(valor_user=user_value)
 
+        #se o usuario depositar, sera retornado um index em string e um valor que sera somado na variavel saldo
         if deposit[0] == "deposit":
             saldo = saldo + deposit[1]
 
+        #se o usuario sacar, sera retornado um index em string e um valor que sera subtraido na variavel saldo
         elif deposit[0] == "Sacar":
             saldo = saldo - deposit[1]
         
